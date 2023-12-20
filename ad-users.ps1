826e7022f981ff7730f7a6b6ad5261765d695304
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2023-12-20 10:43:07 -0500

    Update ad-users.ps1
    updated CheckAccounts function and AccountLockouts variable

--- ad-users.ps1
+++ ad-users.ps1
@@ -1,27 +1,22 @@
 Clear-Host
 
-#If parameter is empty, then outputs the nothing was found message, else it will output the specific accounts
 function CheckAccounts($accounts) {
+    $nothing_message = @("`n*****************************", "*                           *", "*     Nothing was found     *", "*                           *", "*****************************`n")
     if([string]::IsNullOrEmpty($accounts)) {
-        Write-Host "`n*****************************"
-        Write-Host "*                           *"
-        Write-Host "*     Nothing was found     *"
-        Write-Host "*                           *"
-        Write-Host "*****************************`n"
+        foreach ($member in $nothing_message) {
+            Write-Host $member
+        }
     } else {
         $accounts
     }
 }
 
-$CurrentTime = Get-Date -UFormat "%m/%d/%Y %r"
+$CurrentTime = Get-Date
 $PriorDays = (Get-Date).AddDays(-91)
-#Checks to see if any account has a password that expired within the past 91 days (within the last quarter of a year)
 $AccountPasswords = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "DisplayName", "SamAccountName", @{Name="Expiration Date";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Where-Object {$_."Expiration Date" -lt $CurrentTime -and $_."Expiration Date" -gt $PriorDays} | Sort-Object "DisplayName" | Format-Table
+$AccountLockouts = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "lockedOut", "lockoutTime" | Select-Object -Property "DisplayName", "SamAccountName", "lockedOut", @{Name="Lockout Time";Expression={[datetime]::FromFileTime($_."lockoutTime")}} | Where-Object {$_."lockedOut" -eq $True} | Sort-Object "DisplayName" | Format-Table -Property "DisplayName", "SamAccountName", "Lockout Time"
 
-#Checks to see if any account is locked out
-$AccountLockouts = Search-ADAccount -LockedOut | Select-Object Name, SamAccountName, lockedOut | Sort-Object Name | Format-Table
-#Outputs the results through custom function
-Write-Host "Accounts with Expired Passwords:" -ForegroundColor DarkYellow
+Write-Host "Accounts with Expired Passwords:" -ForegroundColor Yellow
 CheckAccounts($AccountPasswords)
-Write-Host "Locked Out Accounts:" -ForegroundColor DarkYellow
+Write-Host "Locked Out Accounts:" -ForegroundColor Yellow
 CheckAccounts($AccountLockouts)
