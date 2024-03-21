dfa818adf2e7b0912ec58aaa2929d54101fe0ff4
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2024-03-21 08:39:21 -0400

    Update master-module.psm1
    Added more commands to the module

--- Modules/master-module.psm1
+++ Modules/master-module.psm1
@@ -1,22 +1,56 @@
-function Get-Distro($DistributionList) {
+# This makes the default value of any command with -AD in the name to use the provided server address
+#
+$PSDefaultParameterValues['*-AD*:Server'] = '{SERVER_NAME}'
+
+# This will output all the email addresses of users who are members of the specified distribution list
+#
+function Get-DistroMembers($DistributionList) {
     (Get-DistributionGroupMember -Identity $DistributionList).PrimarySmtpAddress
 }
 
+# This will output all users that have access to the specified shared mailbox
+#
 function Get-SharedMailbox($SharedMailbox) {
     Get-MailboxPermission -Identity $SharedMailbox | Sort-Object User | Format-Table Identity, User, AccessRights
 }
 
+# This will output a table of all emails sent to the specified user, during the specified time frame
+#
+function Get-EmailTrace($User, $StartTime, $EndTime) {
+    Clear-Host
+
+    Write-Host "Recipient Address:" $User
+    Get-MessageTrace -RecipientAddress $User -StartDate $StartTime -EndDate $EndTime | Format-Table Received, SenderAddress, Subject, Status
+}
+
+# This will create a new dynamic distribution list, based on who their direct report is
+#
+function New-DynamicDistro($DirectManager) {
+    $managerName = ((Get-Culture).TextInfo.ToTitleCase($DirectManager)).replace("."," ")
+    $emailManagerName = $DirectManager.replace(".","")
+    $DistributionName = $managerName + " Direct Reports"
+    $DistributionEmail = $emailManagerName + "-directs@{DOMAIN}"
+    $DistinguishedName = (Get-Recipient -Identity $DirectManager).DistinguishedName
+    $Filter = "Manager -eq '$DistinguishedName'"
+
+    New-DynamicDistributionGroup -Name $DistributionName -PrimarySmtpAddress $DistributionEmail -RecipientFilter $Filter
+}
+
+# This will pull information based on the specified user from the active directory server
+#
 function Get-UserInfo($User) {
     $currentTime = Get-Date -UFormat "%m/%d/%Y %r"
-    $lockedOut = (get-aduser $User -Properties "LockedOut")."LockedOut"
-    $passwordChanged = (get-aduser $User -Properties passwordlastset).PasswordLastSet
-    $tempNewPassword = (get-aduser $User -Properties "msDS-UserPasswordExpiryTimeComputed")."msDS-UserPasswordExpiryTimeComputed"
+    $lockedOut = (Get-ADUser $User -Properties "LockedOut")."LockedOut"
+    $passwordChanged = (Get-ADUser $User -Properties passwordlastset).PasswordLastSet
+    $tempNewPassword = (Get-ADUser $User -Properties "msDS-UserPasswordExpiryTimeComputed")."msDS-UserPasswordExpiryTimeComputed"
     $newPassword = ([datetime]::FromFileTime($tempNewPassword))
-    $adName = (get-aduser $User -Properties Name).Name
-    $treeLocation = (get-aduser $User -Properties CanonicalName).CanonicalName
+    $adName = (Get-ADUser $User -Properties Name).Name
+    $treeLocation = (Get-ADUser $User -Properties CanonicalName).CanonicalName
     $ouIndex = $treeLocation.IndexOf('Active-Users/')
     $ouLocation = (($treeLocation.substring($ouIndex)).Replace('Active-Users/', '')).Replace('/' + $adName, '')
-    Write-Host "`nCurrent Time:" $currentTime
+    Write-Host "*******************************************************"
+    Write-Host "Username:" $User
+    Write-Host "Current Time:" $currentTime
     if ($lockedOut -eq "True") {
         Write-Host "User Locked Out:" $lockedOut -ForegroundColor Red
     } else {
@@ -28,9 +62,12 @@ function Get-UserInfo($User) {
     } else {
         Write-Host "Password Expires:" $newPassword
     }
-    Write-Host "Location: " $ouLocation
+    Write-Host "Location:" $ouLocation
+    Write-Host "*******************************************************"
 }
 
+# This will output every account with an actively expired password from the past 91 days, every account that is actively locked out, and every duplicate account that ends with a number
+#
 function Get-ADLockouts {
     Clear-Host
 
@@ -50,13 +87,87 @@ function Get-ADLockouts {
     Write-Host "Current Time:" $CurrentTime -ForegroundColor Yellow
 }
 
-function CheckString($accounts) {
+# This will update the specified distribution list based on users stored in the specified CSV file
+# Users that are in the file and not the distro will be added. Users that are in the file and the distro, nothing will happen to them. Users that are not in the file and are in the distro will be removed.
+#
+function UpdateDistros($DistributionList, $CSV) {
+    $distroStatus = (Get-DistributionGroup -Identity $DistributionList).DisplayName
+
+    if([string]::IsNullOrEmpty($distroStatus)) {
+        Write-Host "The requested distribution list doesn't exist"
+        Exit
+    } else {
+        Write-Host $distroStatus "exists. Continuing..."
+        Start-Sleep -Seconds 2
+    }
+
+    $updatedUsers = @(Get-Content -Path $CSV)
+    $currentUsers = @((Get-DistributionGroupMember -Identity $DistributionList).PrimarySmtpAddress)
+    $addUsers = @($updatedUsers | Where-Object {$currentUsers -NotContains $_})
+    $removeUsers = @($currentUsers | Where-Object {$updatedUsers -NotContains $_})
+
+    Write-Host "`nIdentity:" $distroStatus
+    Write-Host "Email Address:" $DistributionList
+    Write-Host "`nUsers to be Added:"
+    $addUsers
+    Write-Host "`nUsers to be Removed:"
+    $removeUsers
+
+    $confirmation = Read-Host "`nDo you wish to continue with this process?? (Y/N)"
+
+    if($confirmation -eq "Y" -or $confirmation -eq "y" -or $confirmation -eq "Yes" -or $confirmation -eq "yes" -or $confirmation -eq "YES") {
+        if([string]::IsNullOrEmpty($addUsers)) {
+            Write-Host "No users to add"
+        } else {
+            foreach ($member in $addUsers) {
+                Add-DistributionGroupMember -Identity $DistributionList -Member $member
+                Write-Host "Added" $member "to" $distroStatus
+            }
+        }
+
+        if([string]::IsNullOrEmpty($removeUsers)) {
+            Write-Host "No users to remove"
+        } else {
+            foreach ($member in $removeUsers) {
+                Remove-DistributionGroupMember -Identity $DistributionList -Member $member
+                Write-Host "Removed" $member "from" $distroStatus
+            }
+        }
+    } else {
+        Write-Host "No Actions Taken"
+        Exit
+    }
+}
+
+# This will look through every mailbox in the exchange server and store all the specified stats for items that were archived, due to the retention policy
+#
+function Get-OutlookRetentionReport($ExportLocation) {
+    $mailboxes = @(Get-EXOMailbox -ResultSize Unlimited)
+    $report = @()
+    
+    foreach ($mailbox in $mailboxes) {
+        $inboxstats = Get-EXOMailboxFolderStatistics $mailbox.UserPrincipalName -FolderScope Archive | Where-Object {$_.FolderPath -eq "/Archive"}
+
+        $mbObj = New-Object PSObject
+        $mbObj | Add-Member -MemberType NoteProperty -Name "UPN" -Value $mailbox.UserPrincipalName
+        $mbObj | Add-Member -MemberType NoteProperty -Name "Display Name" -Value $mailbox.DisplayName
+        $mbObj | Add-Member -MemberType NoteProperty -Name "Folder" -Value $inboxstats.FolderPath
+        $mbObj | Add-Member -MemberType NoteProperty -Name "Folder Size (MB)" -Value $inboxstats.FolderandSubFolderSize
+        $mbObj | Add-Member -MemberType NoteProperty -Name "Number of Items" -Value $inboxstats.ItemsinFolderandSubfolders
+        $report += $mbObj
+    }
+    $report | Export-CSV $ExportLocation
+}
+
+# Custom internal function used to verify if the specified variable is empty or not
+#
+function CheckString($Accounts) {
     $nothing_message = @("`n*****************************", "*                           *", "*     Nothing was found     *", "*                           *", "*****************************`n")
-    if([string]::IsNullOrEmpty($accounts)) {
+    if([string]::IsNullOrEmpty($Accounts)) {
         foreach ($member in $nothing_message) {
             Write-Host $member
         }
     } else {
-        $accounts
+        $Accounts
     }
 }
