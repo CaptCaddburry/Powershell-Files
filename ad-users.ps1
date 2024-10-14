c25b22ebc3f063862a93422d31c6b42ce576ea30
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2024-10-14 02:35:34 -0400

    Update ad-users.ps1

--- ad-users.ps1
+++ ad-users.ps1
@@ -1,3 +1,4 @@
+param($DaysPrior = -91)
 Clear-Host
 
 function CheckAccounts($accounts) {
@@ -12,15 +13,17 @@ function CheckAccounts($accounts) {
 }
 
 $CurrentTime = Get-Date
-$PreviousQuarter = (Get-Date).AddDays(-91)
-$AccountPasswords = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "DisplayName", "SamAccountName", @{Name="Expiration Date";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Where-Object {$_."Expiration Date" -lt $CurrentTime -and $_."Expiration Date" -gt $PreviousQuarter} | Sort-Object "DisplayName" | Format-Table
+$PreviousDays = (Get-Date).AddDays($DaysPrior)
+
+$AccountPasswords = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "msDS-UserPasswordExpiryTimeComputed" | Select-Object -Property "DisplayName", "SamAccountName", @{Name="Expiration Date";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Where-Object {$_."Expiration Date" -lt $CurrentTime -and $_."Expiration Date" -gt $PreviousDays} | Sort-Object "DisplayName" | Format-Table
 $AccountLockouts = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "lockedOut", "lockoutTime" | Select-Object -Property "DisplayName", "SamAccountName", "lockedOut", @{Name="Lockout Time";Expression={[datetime]::FromFileTime($_."lockoutTime")}} | Where-Object {$_."lockedOut" -eq $True} | Sort-Object "DisplayName" | Format-Table -Property "DisplayName", "SamAccountName", "Lockout Time"
 $AccountDuplicates = Get-ADUser -Filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} -Properties "DisplayName", "SamAccountName", "WhenCreated", "LastLogon" | Where-Object SamAccountName -like "*[0-9]" | Sort-Object "SamAccountName" | Format-Table -Property "DisplayName", "SamAccountName", "WhenCreated", "LastLogon"
 
 Write-Host "Accounts with Expired Passwords:" -ForegroundColor Yellow
-CheckAccounts($AccountPasswords)
+CheckAccounts $AccountPasswords
 Write-Host "Locked Out Accounts:" -ForegroundColor Yellow
-CheckAccounts($AccountLockouts)
+CheckAccounts $AccountLockouts
 Write-Host "Duplicate Accounts:" -ForegroundColor Yellow
-CheckAccounts($AccountDuplicates)
+CheckAccounts $AccountDuplicates
+
 Write-Host "Current Time:" $CurrentTime -ForegroundColor Yellow
