559933c9c530df917a8549c9f0f07c1e552fd281
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2025-12-03 12:56:00 -0500

    Delete Intune Scripts directory

--- Intune Scripts/disablesearchboxsuggestions.ps1
+++
@@ -1,9 +0,0 @@
-$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer'
-$Name = 'DisableSearchBoxSuggestions'
-$Value = '1'
-
-If (!(Test-Path $RegistryPath)) {
-    New-Item -Path $RegistryPath -Force
-}
-
-New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
