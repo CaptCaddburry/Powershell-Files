4d5786aa1fa438358beed6d90c53f95805df008b
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2026-02-09 08:01:03 -0500

    Create uptime-reboot-task.ps1
    Creates a task on the local device to check uptime at every startup

---
+++ Intune Scripts/Windows Uptime/uptime-reboot-task.ps1
@@ -0,0 +1,24 @@
+$taskName = "WeeklyUptimeRebootCheck"
+
+$scriptBlock = {
+    $uptimeDays = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    $uptime = (New-TimeSpan -Start `$uptimeDays -End (Get-Date)).Days
+    if ($uptime -ge 7) {
+        shutdown.exe /r /t 900 /c 'IT Notice: Automatic Restart in 15 minute(s). Save your work now.' /d p:5:19
+    }
+}
+
+$encodedScript = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($scriptBlock))
+$action = New-ScheduledTaskAction `
+    -Execute "powershell.exe" `
+    -Argument "-NoProfile -ExecutionPolicy Bypass -EncodedCommand $encodedScript"
+
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
+
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $action `
+    -Trigger $trigger `
+    -Principal $principal `
+    -Force
