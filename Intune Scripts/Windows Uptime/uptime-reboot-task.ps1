733a770ae504738370306b87a4a16ff0d81f577f
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2026-02-10 14:06:11 -0500

    Update uptime-reboot-task.ps1
    Forgot my if statement.....

--- Intune Scripts/Windows Uptime/uptime-reboot-task.ps1
+++ Intune Scripts/Windows Uptime/uptime-reboot-task.ps1
@@ -1,25 +1,29 @@
 $taskName = "WeeklyUptimeRebootCheck"
 
 $scriptBlock = {
-    $Minutes = 15
-    $Seconds = [Math]::Max(60, $Minutes * 60)
-    $ETA = (Get-Date).AddSeconds($Seconds).ToString("hh:mm tt")
-    $XML = @"
-        <toast duration="long">
-            <visual>
-                <binding template="ToastGeneric">
-                    <text>Windows Restart Notification</text>
-                    <text>Your computer needs to be rebooted for security purposes. Your computer will restart at $ETA. Please save your work now.</text>
-                </binding>
-            </visual>
-        </toast>
+    $uptimeDays = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    $uptime = (New-TimeSpan -Start $uptimeDays -End (Get-Date)).Days
+    if ($uptime -ge 7) {
+        $Minutes = 15
+        $Seconds = [Math]::Max(60, $Minutes * 60)
+        $ETA = (Get-Date).AddSeconds($Seconds).ToString("hh:mm tt")
+        $XML = @"
+            <toast duration="long">
+                <visual>
+                    <binding template="ToastGeneric">
+                        <text>Windows Restart Notification</text>
+                        <text>Your computer needs to be rebooted for security purposes. Your computer will restart at $ETA. Please save your work now.</text>
+                    </binding>
+                </visual>
+            </toast>
 "@
-    $XMLDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
-    $XMLDocument.LoadXml($XML)
-    $AppId = (Get-StartApps | Where -Property Name -EQ "Windows PowerShell").AppId
-    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XMLDocument)
-    $ShutdownComment = "IT Notice: Automatic Restart in $Minutes minute(s). Save your work now."
-    & shutdown /r /t $Seconds /c $ShutdownComment /d p:5:19
+        $XMLDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+        $XMLDocument.LoadXml($XML)
+        $AppId = (Get-StartApps | Where -Property Name -EQ "Windows PowerShell").AppId
+        [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XMLDocument)
+        $ShutdownComment = "IT Notice: Automatic Restart in $Minutes minute(s). Save your work now."
+        & shutdown /r /t $Seconds /c $ShutdownComment /d p:5:19
+    }
 }
 
 $encodedScript = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($scriptBlock))
