74d2dcfe19a5a68118ccce0bd8ef9cea956cc907
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2026-02-10 10:23:16 -0500

    Update uptime-reboot-task.ps1
    Updated scriptBlock logic to copy the same logic from the remediation script

--- Intune Scripts/Windows Uptime/uptime-reboot-task.ps1
+++ Intune Scripts/Windows Uptime/uptime-reboot-task.ps1
@@ -1,11 +1,25 @@
 $taskName = "WeeklyUptimeRebootCheck"
 
 $scriptBlock = {
-    $uptimeDays = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
-    $uptime = (New-TimeSpan -Start $uptimeDays -End (Get-Date)).Days
-    if ($uptime -ge 7) {
-        shutdown.exe /r /t 900 /c 'IT Notice: Automatic Restart in 15 minute(s). Save your work now.' /d p:5:19
-    }
+    $Minutes = 15
+    $Seconds = [Math]::Max(60, $Minutes * 60)
+    $ETA = (Get-Date).AddSeconds($Seconds).ToString("hh:mm tt")
+    $XML = @"
+        <toast duration="long">
+            <visual>
+                <binding template="ToastGeneric">
+                    <text>Windows Restart Notification</text>
+                    <text>Your computer needs to be rebooted for security purposes. Your computer will restart at $ETA. Please save your work now.</text>
+                </binding>
+            </visual>
+        </toast>
+"@
+    $XMLDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+    $XMLDocument.LoadXml($XML)
+    $AppId = (Get-StartApps | Where -Property Name -EQ "Windows PowerShell").AppId
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XMLDocument)
+    $ShutdownComment = "IT Notice: Automatic Restart in $Minutes minute(s). Save your work now."
+    & shutdown /r /t $Seconds /c $ShutdownComment /d p:5:19
 }
 
 $encodedScript = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($scriptBlock))
